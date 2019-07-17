b8fbc118234445295028a10a60134a5f387a1ee4
Veronica <veronica.21.coutts@gmail.com>
2019-07-17 17:54:19 +0200

    WIP vault
    Comments needed
    some functionality outstanding

--- Blockchain/contracts/Vault.sol
+++ Blockchain/contracts/Vault.sol
@@ -1,15 +1,95 @@
 pragma solidity ^0.5.8;
 
+import "./interfaces/IERC20.sol";
+
 contract Vault {
+    //The collateral goals for each round
+    uint256[] public fundingGoals;
+    //The length of each funding round
+    uint256[] public phaseDurations;
+    //The creator/researcher/owner
+    address public creator;
+    //The factory that created this market
+    address public factory;
+    //The collateral of this vault (token)
+    address public collateral;
+    //The current phase of funding the vault is on
+    uint8 public currentPhase = 0;
+    //The market assosiated with this vault
+    address public market;
+    //The market start time
+    uint256 public startTime;
 
     constructor(
         uint256[] memory _fundingGoals,
         uint256[] memory _phaseDurations,
-        address _creator
+        address _creator,
+        address _colalteral
     )
         public
     {
-
+        // Checks there are the same number of goals as phases
+        //  as each goal needs a phase and visa versa.
+        require(
+            _fundingGoals.length == _phaseDurations.length,
+            "There must be the same number of funding goals as phases"
+        );
+        //Setting contract variables
+        fundingGoals = _fundingGoals;
+        phaseDurations = _phaseDurations;
+        creator = _creator;
+        collateral = _colalteral;
+        factory = msg.sender;
     }
 
+    modifier onlyCreator() {
+        require(msg.sender == creator || msg.sender == factory, "Only the creator of a market can withdraw milestones");
+        _;
+    }
+
+    function updateCreator(address _newCreator) 
+        public
+    {
+        require(msg.sender == factory, "Only the deploying factory can change creator address");
+        creator = _newCreator;
+    }
+
+    function startPhase(address _market)
+        public
+        onlyCreator()
+    {
+        market = _market;
+        startTime = block.timestamp;
+    }
+
+    function withdraw()
+        public
+        onlyCreator()
+    {
+        //Checks that the phase time has not been exceeded.
+        require(
+            phaseDurations[currentPhase] >= block.timestamp,
+            "The phase time has expired for this round. Market should be ended"
+        );
+        //Checks that the collateral goal has been met.
+        require(
+            IERC20(collateral).balanceOf(address(this)) >= fundingGoals[currentPhase],
+            "The collateral goal has not been met, but market is still within fundrasing phase"
+        );
+        //Ensures that the transfer suceesds.
+        require(IERC20(collateral).transfer(creator, fundingGoals[currentPhase]), "Transfer failed.");
+        //Increases the phase the project is on
+        currentPhase = currentPhase + 1;
+    }
+
+    function withdrawAndClose()
+        public
+        onlyCreator()
+    {
+        require(
+            currentPhase == fundingGoals.length,
+            "Market is not in final round, please use withdraw"
+        );
+
+    }
 }
