85f1e351f2b46b5347c71f0787fe98afeb25ca7a
Benjamin Scholtz <bscholtz.bds@gmail.com>
2019-07-25 17:42:45 +0200

    Add initial Solidity contract

--- Blockchain/contracts/_curveIntegrals/v1/ICurveFunctions.sol
+++ Blockchain/contracts/_curveIntegrals/v1/ICurveFunctions.sol
@@ -1,18 +1,14 @@
 pragma solidity 0.5.9;
 
 /// @author Veronica @ Protea
-/// @title Vyper bonding curve functions interface 
+/// @title Bonding curve functions interface 
 
 interface ICurveFunctions {
-    /// @dev            Calculating the curves integral
-    /// @param x_t      ?
-    /// @param gradientDenominator The gradeint of the curve
-    /// @param b        ?
-    function curveIntegral(uint256 x_t, uint256 gradientDenominator, uint256 b) external pure returns(uint256);
+    /// @dev            Calculates the definite integral of the curve
+    /// @param x_t      token value for upper limit of definite integral
+    function curveIntegral(uint256 x_t) external pure returns(uint256);
 
-    /// @dev            Calculates the inverse integral of the curve
-    /// @param x_d      ?
-    /// @param gradientDenominator The gradeint of the curve
-    /// @param b        ?
-    function inverseCurveIntegral(uint256 x_d, uint256 gradientDenominator, uint256 b) external pure returns(uint256);
+    /// @dev            Calculates the definite inverse integral of the curve
+    /// @param x_d      collateral value for upper limit of definite integral
+    function inverseCurveIntegral(uint256 x_d) external pure returns(uint256);
 }
