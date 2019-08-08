c8b80a53ddba5927db9ccc59cc6efe3d22f9a171
Michael Yankelev <myankelev@gmail.com>
2019-08-08 12:57:18 +0200

    wire up saga

--- WebApp/app/containers/AdminProjectReviewContainer/saga.ts
+++ WebApp/app/containers/AdminProjectReviewContainer/saga.ts
@@ -2,27 +2,30 @@ import * as adminUserActions from './actions'
 import { takeEvery, select, call } from 'redux-saga/effects';
 import { getType } from 'typesafe-actions';
 import { ApplicationRootState } from 'types';
-import { promoteToAdmin as promoteToAdminApi } from '../../api'
+import { 
+  approveProject as approveProjectApi,
+  rejectProject as rejectProjectApi,
+ } from '../../api'
 import { forwardTo } from 'utils/history';
 
 export function* approveProject(action) {
   const apiKey = yield select((state: ApplicationRootState) => state.authentication.accessToken);
-  // try {
-  //   yield call(promoteToAdminApi, action.payload, apiKey);
-  //   yield call(forwardTo, '/admin');
-  // } catch (error) {
-  //   console.log(error);
-  // }
+  try {
+    yield call(approveProjectApi, action.payload, apiKey);
+    yield call(forwardTo, '/admin/project');
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export function* rejectProject(action) {
   const apiKey = yield select((state: ApplicationRootState) => state.authentication.accessToken);
-  // try {
-  //   yield call(promoteToAdminApi, action.payload, apiKey);
-  //   yield call(forwardTo, '/admin');
-  // } catch (error) {
-  //   console.log(error);
-  // }
+  try {
+    yield call(rejectProjectApi, action.payload, apiKey);
+    yield call(forwardTo, '/admin/project');
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export default function* createProjectContainerWatcherSaga() {
