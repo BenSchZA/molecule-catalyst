7a9f542124b6ade9b37234c6f5945b13ebff0825
Michael Yankelev <myankelev@gmail.com>
2019-08-11 22:54:20 +0200

    wire up filter state update reducer

--- WebApp/app/containers/DiscoverContainer/reducer.ts
+++ WebApp/app/containers/DiscoverContainer/reducer.ts
@@ -18,7 +18,12 @@ export const initialState: ContainerState = {
 function projectReducer(state: ContainerState = initialState, action: ContainerActions ) {
   switch (action.type) {
     case getType(DiscoverActions.setFilter):
-      return {...state};
+      return {...state,
+        filter: {
+          ...state.filter,
+          ...action.payload,
+        }
+      };
     default:
       return state;
   }
