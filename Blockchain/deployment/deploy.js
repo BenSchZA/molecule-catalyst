1785f6d5f2a2b35406cbfbf57728f62555784852
Benjamin Scholtz <bscholtz.bds@gmail.com>
2019-08-12 13:57:05 +0200

    Basic deployment script created

--- Blockchain/deployment/deploy.js
+++ Blockchain/deployment/deploy.js
@@ -1,11 +1,58 @@
-const etherlime = require('etherlime');
-const LimeFactory = require('../build/LimeFactory.json');
+const etherlime = require('etherlime-lib');
+//const LimeFactory = require('../build/LimeFactory.json');
 
+
+const PseudoDaiABI = require('../build/PseudoDaiToken.json');
+
+const MoleculeVaultABI = require('../build/MoleculeVault.json');
+
+const CurveFunctionsABI = require('../build/CurveFunctions.json');
+const CurveRegistryABI = require('../build/CurveRegistry.json');
+const MarketRegistryABI = require('../build/MarketRegistry.json');
+const MarketFactoryABI = require('../build/MarketFactory.json');
+
+const defaultConfigs = {
+  gasPrice: 1000000000,
+  gasLimit: 4700000
+};
 
 const deploy = async (network, secret) => {
 
 	const deployer = new etherlime.EtherlimeGanacheDeployer();
-	const result = await deployer.deploy(LimeFactory);
+
+  const pseudoDaiInstance = await deployer.deploy(
+    PseudoDaiABI,
+    false,
+    "PseudoDai",
+    "pDAI",
+    18
+  );
+
+  const moleculeVaultInstance = await deployer.deploy(
+    MoleculeVaultABI,
+    false,
+    pseudoDaiInstance.contract.address,
+    15,
+    deployer.signer.address
+  );
+
+  const curveFunctionsInstance = await deployer.deploy(CurveFunctionsABI);
+  const curveRegistryInstance = await deployer.deploy(CurveRegistryABI);
+  const registerCurveTX = await curveRegistryInstance.registerCurve(
+    curveFunctionsInstance.contract.address,
+    "linear: (1/17500)*x + 0.5"
+  );
+  let result = await curveRegistryInstance.verboseWaitForTransaction(registerCurveTX, 'Register curve');
+
+  const marketRegistryInstance = await deployer.deploy(MarketRegistryABI);
+  const marketFactoryInstance = await deployer.deploy(
+    MarketFactoryABI,
+    false,
+    pseudoDaiInstance.contract.address,
+    moleculeVaultInstance.contract.address,
+    marketRegistryInstance.contract.address,
+    curveRegistryInstance.contract.address
+  );
 
 };
 
