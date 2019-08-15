6de83b0c543addc9b3c733b388569439e7900b84
Michael Yankelev <myankelev@gmail.com>
2019-08-15 16:12:36 +0200

    fix curve tests

--- Blockchain/test/curveIntegrals/v1/curveFunctions.test.js
+++ Blockchain/test/curveIntegrals/v1/curveFunctions.test.js
@@ -1,67 +1,63 @@
-const { 
-    PseudoDaiTokenAbi,
-    MoleculeVaultAbi, 
-    MarketRegistryAbi, 
-    CurveRegistryAbi, 
-    CurveFunctionsAbi, 
-    MarketFactoryAbi, 
-    ethers, 
-    etherlime, 
-    daiSettings,
-    moleculeVaultSettings,
-    marketSettings,
-    MarketAbi,
-    VaultAbi,
-    defaultDaiPurchase,
-    defaultTokenVolume,
-    purchasingSequences,
-    simulatedCurve
- } = require("../../testing.settings.js");
+const {
+  PseudoDaiTokenAbi,
+  MoleculeVaultAbi,
+  MarketRegistryAbi,
+  CurveRegistryAbi,
+  CurveFunctionsAbi,
+  MarketFactoryAbi,
+  ethers,
+  etherlime,
+  daiSettings,
+  moleculeVaultSettings,
+  marketSettings,
+  MarketAbi,
+  VaultAbi,
+  defaultDaiPurchase,
+  defaultTokenVolume,
+  purchasingSequences,
+  simulatedCurve
+} = require("../../testing.settings.js");
 
 describe('Curve Integral test', () => {
-    let molAdmin = accounts[1];
-    let creator = accounts[2];
-    let user1 = accounts[3];
-    let user2 = accounts[4];
-    let pseudoDaiInstance, moleculeVaultInstance, curveRegistryInstance, marketRegistryInstance, marketFactoryInstance, curveIntegralInstance;
+  let molAdmin = accounts[1];
+  let creator = accounts[2];
+  let user1 = accounts[3];
+  let user2 = accounts[4];
+  let pseudoDaiInstance, moleculeVaultInstance, curveRegistryInstance, marketRegistryInstance, marketFactoryInstance, curveIntegralInstance;
 
-  
-    beforeEach('', async () => {
-        deployer = new etherlime.EtherlimeGanacheDeployer(molAdmin.secretKey);
 
-        curveFunctionsInstance = await deployer.deploy(
-            CurveFunctionsAbi,
-            false
-        );
+  beforeEach('', async () => {
+    deployer = new etherlime.EtherlimeGanacheDeployer(molAdmin.secretKey);
 
+    curveFunctionsInstance = await deployer.deploy(
+      CurveFunctionsAbi,
+      false
+    );
+
+  });
+
+  describe('Curve checks', () => {
+    it('Curve Integral functions as expected', async () => {
+      const rawDaiBN = await curveFunctionsInstance.from(molAdmin)
+        .curveIntegral(simulatedCurve.defaultPurchaseAmount)
+      assert.equal(
+        ethers.utils.formatUnits(rawDaiBN, 18),
+        50.285714285714285,
+        "Calculation incorrect"
+      )
     });
 
-    describe('Curve checks', () => {
-        it('Curve Integral functions as expected', async () => {
-            const rawDaiBN = await curveFunctionsInstance.from(molAdmin)
-                .curveIntegral(simulatedCurve.defaultPurchaseAmount, simulatedCurve.gradientDenominator, simulatedCurve.scaledShift)
-            assert.equal(
-                ethers.utils.formatUnits(rawDaiBN, 18),
-                50.285714,
-                "Calculation incorrect"
-            )
-        });
-
-        it('Inverse Curve Integral functions as expected', async () =>{
-            const rawDaiBN = await curveFunctionsInstance.from(molAdmin)
-                .curveIntegral(simulatedCurve.defaultPurchaseAmount, simulatedCurve.gradientDenominator, simulatedCurve.scaledShift)
-            const rawTokenBN = await curveFunctionsInstance.from(molAdmin)
-                .inverseCurveIntegral(
-                    rawDaiBN, 
-                    simulatedCurve.gradientDenominator, 
-                    simulatedCurve.scaledShift
-                )
-            assert.equal(
-              ethers.utils.formatUnits(rawTokenBN, 18),
-              99.9999982499,
-              "Calculation incorrect"
-            )
-        });
-        
+    it('Inverse Curve Integral functions as expected', async () => {
+      const rawDaiBN = await curveFunctionsInstance.from(molAdmin)
+        .curveIntegral(simulatedCurve.defaultPurchaseAmount)
+      const rawTokenBN = await curveFunctionsInstance.from(molAdmin)
+        .inverseCurveIntegral(rawDaiBN)
+      assert.equal(
+        ethers.utils.formatUnits(rawTokenBN, 18),
+        99.999999999999982499,
+        "Calculation incorrect"
+      )
     });
+
+  });
 })
