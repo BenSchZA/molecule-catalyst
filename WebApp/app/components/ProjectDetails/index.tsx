9512c9555c1ee470c1e67688a841bd234e12d4fb
Michael Yankelev <myankelev@gmail.com>
2019-08-22 17:10:19 +0200

    wire up banner image

--- WebApp/app/components/ProjectDetails/index.tsx
+++ WebApp/app/components/ProjectDetails/index.tsx
@@ -9,6 +9,7 @@ import { Theme, createStyles, withStyles, WithStyles, Container, Typography, But
 import { Project, ProjectSubmissionStatus } from 'domain/projects/types';
 import apiUrlBuilder from 'api/apiUrlBuilder';
 import { Face } from '@material-ui/icons';
+import { colors } from 'theme';
 
 const styles = ({ spacing }: Theme) =>
   createStyles({
@@ -19,6 +20,36 @@ const styles = ({ spacing }: Theme) =>
     researcherAvatar: {
       height: 120,
       width: 120,
+    },
+    bannerWrapper: {
+      position: 'relative',
+      "&:after": {
+        content: "''",
+        display: "block",
+        position: "absolute",
+        top: 0,
+        left: 0,
+        height: '100%',
+        width: '100%',
+        background: '#003E52',
+        zIndex: 1,
+        opacity: 0.69,
+      }
+    },
+    bannerImage: {
+      width: '100%',
+    },
+    bannerContent: {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      position: 'absolute',
+      left: '50%',
+      top: '50%',
+      transform: 'translate(-50%, -50%)',
+      zIndex: 2,
+      color: colors.white
     }
   });
 
@@ -27,60 +58,68 @@ interface OwnProps extends WithStyles<typeof styles> {
 }
 
 const ProjectDetails: React.FunctionComponent<OwnProps> = ({ project, classes }: OwnProps) => (
-  (project) ? 
-  <Container maxWidth='lg'>
-    <Paper className={classes.projectSection}>
-      <Typography variant='h4'>{project.title}</Typography>
-      <Typography variant='h6'>Researcher: {project.user.fullName}</Typography>
-      <Typography variant='h6'>Organization: {project.user.affiliatedOrganisation}</Typography>
-      <Typography variant='h6'>Status: {ProjectSubmissionStatus[project.status]}</Typography>
-      <img src={apiUrlBuilder.attachmentStream(project.featuredImage)} />
-      <Button onClick={() => console.log('buy')}>Support Project</Button>
-      <Button onClick={() => console.log('sell')}>Redeem Holdings</Button>
-      <Divider />
-      <Typography variant='h6'>Abstract: {project.abstract}</Typography>
-    </Paper>
-    <Paper className={classes.projectSection}>
-      <Typography variant='h4'>Research Background</Typography>
-      <Typography variant='subtitle2'>What is the significance of your research</Typography>
-      <Typography>{project.context}</Typography>
-      <Typography variant='subtitle2'>What is the experimental approach for this reseach initiative</Typography>
-      <Typography>{project.approach}</Typography>
-    </Paper>
-    <Paper className={classes.projectSection}>
-      <Typography variant='h4'>Team</Typography>
-      <Typography variant='subtitle2'>Reseacher</Typography>
-      <Grid container direction='row'>
-        <Grid item>
-          <Typography variant='body1'>{project.user.fullName || ''}</Typography>
-          <Typography variant='body2'>{project.user.professionalTitle || ''}</Typography>
-          <Typography variant='body2'>{project.user.affiliatedOrganisation || ''}</Typography>
+  (project) ?
+    <Container maxWidth='lg'>
+      <div className={classes.bannerWrapper}>
+        <img src={apiUrlBuilder.attachmentStream(project.featuredImage)} className={classes.bannerImage} />
+        <div className={classes.bannerContent}>
+          <Typography variant='h4'>{project.title}</Typography>
+          <div>
+            <Button onClick={() => console.log('buy')}>Support Project</Button>
+            <Button onClick={() => console.log('sell')}>Redeem Holdings</Button>
+          </div>
+        </div>
+      </div>
+      <Paper className={classes.projectSection}>
+
+        <Typography variant='h6'>Researcher: {project.user.fullName}</Typography>
+        <Typography variant='h6'>Organization: {project.user.affiliatedOrganisation}</Typography>
+        <Typography variant='h6'>Status: {ProjectSubmissionStatus[project.status]}</Typography>
+
+        <Divider />
+        <Typography variant='h6'>Abstract: {project.abstract}</Typography>
+      </Paper>
+      <Paper className={classes.projectSection}>
+        <Typography variant='h4'>Research Background</Typography>
+        <Typography variant='subtitle2'>What is the significance of your research</Typography>
+        <Typography>{project.context}</Typography>
+        <Typography variant='subtitle2'>What is the experimental approach for this reseach initiative</Typography>
+        <Typography>{project.approach}</Typography>
+      </Paper>
+      <Paper className={classes.projectSection}>
+        <Typography variant='h4'>Team</Typography>
+        <Typography variant='subtitle2'>Reseacher</Typography>
+        <Grid container direction='row'>
+          <Grid item>
+            <Typography variant='body1'>{project.user.fullName || ''}</Typography>
+            <Typography variant='body2'>{project.user.professionalTitle || ''}</Typography>
+            <Typography variant='body2'>{project.user.affiliatedOrganisation || ''}</Typography>
+          </Grid>
+          <Grid item>
+            <Avatar className={classes.researcherAvatar} src={project.user.profileImage && apiUrlBuilder.attachmentStream(project.user.profileImage)}>
+              {!project.user.profileImage && <Face fontSize='large' />}
+            </Avatar>
+          </Grid>
+          <Grid item>
+            <Typography>{project.user.biography}</Typography>
+          </Grid>
         </Grid>
-        <Grid item>
-          <Avatar className={classes.researcherAvatar} src={project.user.profileImage && apiUrlBuilder.attachmentStream(project.user.profileImage)}>
-            {!project.user.profileImage && <Face fontSize='large' />}
-          </Avatar>
-        </Grid>
-        <Grid item>
-          <Typography>{project.user.biography}</Typography>
-        </Grid>
-      </Grid>
-      <Typography variant='subtitle2'>Contributors</Typography>
-      <Table>
-        <TableBody>
-          {project.collaborators && project.collaborators.map((c, i) =>
-            <TableRow key={i}>
-              <TableCell>{c.fullName}</TableCell>
-              <TableCell>{c.professionalTitle}</TableCell>
-              <TableCell>{c.affiliatedOrganisation}</TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
-    </Paper>
-  </Container> :
-  <Container>
-    Loading data
+        <Typography variant='subtitle2'>Contributors</Typography>
+        <Table>
+          <TableBody>
+            {project.collaborators && project.collaborators.map((c, i) =>
+              <TableRow key={i}>
+                <TableCell>{c.fullName}</TableCell>
+                <TableCell>{c.professionalTitle}</TableCell>
+                <TableCell>{c.affiliatedOrganisation}</TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </Paper>
+    </Container> :
+    <Container>
+      Loading data
   </Container>
 );
 
