5ea8d16e4afeb0247ae54169a3b4b490f0742772
Benjamin Scholtz <contact@benjaminscholtz.com>
2019-09-14 11:47:58 +0000

    Hotfix: Update env example and Nix deps

--- .environments/shell.nix
+++ .environments/shell.nix
@@ -5,7 +5,7 @@ stdenv.mkDerivation {
   buildInputs = [
     docker
     docker_compose
-    nodejs-10_x
+    nodejs-11_x
     yarn
     pkgconfig
     autoconf
@@ -19,12 +19,12 @@ stdenv.mkDerivation {
     nasm
     autogen
     zlib
-    nodePackages_10_x.node-gyp
-    nodePackages_10_x.node-gyp-build
-    nodePackages_10_x.node-pre-gyp
-    #python36
-    #python36Packages.pip
-    #python36Packages.setuptools
+    nodePackages.node-gyp
+    nodePackages.node-gyp-build
+    nodePackages.node-pre-gyp
+    python
+    pythonPackages.pip
+    pythonPackages.setuptools
     kubectl
     git
     awscli
