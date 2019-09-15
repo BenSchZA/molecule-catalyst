4bbe09f8de90f10c24ab0fc2ff66d0ae715090bd
Benjamin Scholtz <contact@benjaminscholtz.com>
2019-09-15 20:14:52 +0000

    Hotfix: Add SSL link to institution link

--- WebApp/app/components/ProjectCard/index.tsx
+++ WebApp/app/components/ProjectCard/index.tsx
@@ -173,7 +173,7 @@ const ProjectCard: React.FunctionComponent<OwnProps> = ({ project, classes }: Ow
         <div className={classes.association}>{project.user.affiliatedOrganisation}</div>
         </div>
         <div className={classes.avatar}>
-          <Avatar src='http://www.staff.uct.ac.za/sites/default/files/image_tool/images/431/services/comms_marketing/branding/logo_downloads/transparent_round_logo.gif'></Avatar>
+          <Avatar src='https://www.staff.uct.ac.za/sites/default/files/image_tool/images/431/services/comms_marketing/branding/logo_downloads/transparent_round_logo.gif'></Avatar>
         </div>
       </CardActions>
     </Card>
