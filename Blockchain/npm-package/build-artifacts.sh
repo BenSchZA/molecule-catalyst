977a00a9e05f124d59464fa1e168201a0bf18a57
Benjamin Scholtz <contact@benjaminscholtz.com>
2019-10-24 22:30:13 +0200

    Upgrade contract version

--- Blockchain/npm-package/build-artifacts.sh
+++ Blockchain/npm-package/build-artifacts.sh
@@ -4,7 +4,7 @@ set -o errexit -o nounset -o pipefail
 
 # This is necessary because of the way Etherlime compiles the Vyper contracts, and relative directories
 cp -r ../contracts temp
-etherlime compile --workingDirectory=temp --buildDirectory=artifacts --exportAbi
+etherlime compile --workingDirectory=temp --buildDirectory=artifacts --exportAbi --solc-version=0.5.10
 rm ./artifacts/*.json
 mv ./artifacts/abis/*.json ./artifacts
 rm -rf ./artifacts/abis
