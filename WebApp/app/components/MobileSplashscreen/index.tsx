75a4e7893321f6c5b0c25f177323fccc485fc652
Michael Yankelev <myankelev@gmail.com>
2019-11-06 13:26:15 +0200

    more styling

--- WebApp/app/components/MobileSplashscreen/index.tsx
+++ WebApp/app/components/MobileSplashscreen/index.tsx
@@ -18,21 +18,27 @@ const styles = (theme: Theme) =>
     },
     text: {
       color: '#FFF'
+    },
+    link: {
+      color: '#FFF',
+      textDecoration: 'underline',
     }
   });
 
 interface OwnProps extends WithStyles<typeof styles> { }
 
-const MobileSplashscreen: React.FC<OwnProps> = ({classes}: OwnProps) =>
+const MobileSplashscreen: React.FC<OwnProps> = ({ classes }: OwnProps) =>
   <div className={classes.background}>
     <ReactSVG src="/molecule-catalyst-logo.svg" beforeInjection={(svg) => svg.setAttribute('style', 'width: 300px')} />
-    <Typography className={classes.text}>
-      This application is not accessible on mobile devices. Please try again from a desktop device.
-    </Typography>
-    <hr />
-    <Link href='https://catalyst.molecule.to/' className={classes.text}>
-      More Information
-    </Link>
+    <div>
+      <Typography className={classes.text}>
+        This application is not accessible on mobile devices. Please try again from a desktop device.
+      </Typography>
+      <hr />
+      <Link href='https://catalyst.molecule.to/' className={classes.link}>
+        More Information
+      </Link>
+    </div>
   </div>;
 
 export default withStyles(styles, { withTheme: true })(MobileSplashscreen);
