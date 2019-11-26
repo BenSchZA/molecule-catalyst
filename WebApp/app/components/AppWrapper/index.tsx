56c0d15e19dc842943615468e3d194a80e5620ce
Benjamin Scholtz <contact@benjaminscholtz.com>
2019-11-26 09:53:10 +0000

    Resolve "UX: Trucate text and/or scroll to top button"

--- WebApp/app/components/AppWrapper/index.tsx
+++ WebApp/app/components/AppWrapper/index.tsx
@@ -1,5 +1,5 @@
 import React, { Fragment, useState } from 'react';
-import { List, ListItem, Button, Menu, MenuItem, Avatar, Container, Tooltip, Typography } from '@material-ui/core';
+import { List, ListItem, Button, Menu, MenuItem, Avatar, Container, Tooltip, Typography, Fab } from '@material-ui/core';
 import { createStyles, withStyles, WithStyles, Theme } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -12,6 +12,7 @@ import { forwardTo } from 'utils/history';
 import { UserType } from 'containers/App/types';
 import ErrorBoundary from 'containers/ErrorBoundary';
 import AppFooter from 'components/AppFooter';
+import { ExpandLess } from '@material-ui/icons';
 
 const spacingFromProfile = 20;
 const footerHeight = 300;
@@ -32,6 +33,7 @@ const styles = ({ spacing, zIndex, mixins }: Theme) => createStyles({
   body: {
     height: "100%",
     margin: "0",
+    scrollBehavior: 'smooth',
   },
   content: {
     paddingTop: spacing(8),
@@ -94,6 +96,11 @@ const styles = ({ spacing, zIndex, mixins }: Theme) => createStyles({
       zIndex: 0
     }
   },
+  fab: {
+    position: 'fixed',
+    bottom: '20px',
+    left: '20px',
+  }
 });
 
 interface OwnProps extends WithStyles<typeof styles> {
@@ -215,6 +222,9 @@ const AppWrapper: React.FunctionComponent<Props> = ({
         </main>
         <AppFooter />
       </ErrorBoundary>
+      <Fab className={classes.fab} color="primary" aria-label="scroll-to-top" onClick={() => window.scrollTo(0, 0)}>
+        <ExpandLess />
+      </Fab>
     </div>
   );
 }
